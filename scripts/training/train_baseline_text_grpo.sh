7e6fec780e70010d9dcc813b5e5d1cc6c8df23ae
Xinwu Ye <1010647987@qq.com>
2026-03-30 00:48:58 +0800

    Reduce baseline text GRPO lengths

--- scripts/training/train_baseline_text_grpo.sh
+++ scripts/training/train_baseline_text_grpo.sh
@@ -30,8 +30,8 @@ accelerate launch --multi_gpu --num_processes 8 train_grpo_text.py \
   --grad_accum 1 \
   --lr 1e-5 \
   --epochs 1 \
-  --max_prompt_length 2048 \
-  --max_completion_length 2048 \
+  --max_prompt_length 1024 \
+  --max_completion_length 1024 \
   --num_generations 8 \
   --num_iterations 1 \
   --beta 0.0 \
@@ -39,7 +39,7 @@ accelerate launch --multi_gpu --num_processes 8 train_grpo_text.py \
   --use_vllm \
   --vllm_mode colocate \
   --vllm_gpu_memory_utilization 0.3 \
-  --vllm_max_model_len 4096 \
+  --vllm_max_model_len 2048 \
   --temperature 1.5 \
   --gradient_checkpointing \
   "$@"
